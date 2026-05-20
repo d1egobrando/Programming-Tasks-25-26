50b599fe34099ece1ac7f930071d44cdad4dbd16
d1egobrando <45485@rws.uk.net>
2026-05-20 13:37:39 +0100

    Update 02_min_max_finder.py

--- Week_01_Core_Fundamentals/02_min_max_finder.py
+++ Week_01_Core_Fundamentals/02_min_max_finder.py
@@ -1,23 +1,55 @@
-"""
-TASK: 02 Min Max Finder
+def find_min_max():
+    num1 = int(input("Enter a number:"))
+    num2 = int(input("Enter a number:"))
+    num3 = int(input("Enter a number:"))
+    num4 = int(input("Enter a number:"))
+    num5 = int(input("Enter a number:"))
 
-# Min/Max Finder
-Write a program that:
-- Accepts a list of integers.
-- Manually finds the min and max (no built-in min/max).
-- Includes a function `find_min_max(values)` returning `(min_value, max_value)`.
+    List = [num1,num2,num3,num4,num5]
 
-TODO:
-- Fill in functions
-- Add demonstration code under `if __name__ == "__main__":`
-"""
+    print(List)
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
+    max_value=num5
+    min_value=num1
+
+    if num1 < min_value:
+        min_value = num1
+
+    if num2 < min_value:
+        min_value = num2
+
+    if num3 < min_value:
+        min_value = num3
+
+    if num4 < min_value:
+        min_value = num4
+
+    if num5 < min_value:
+        min_value = num5
+
+    print(min_value)
+
+    if num1 > max_value:
+        max_value = num1
+
+    if num2 > max_value:
+        max_value = num2
+
+    if num3 > max_value:
+        max_value = num3
+
+    if num4 > max_value:
+        max_value = num4
+
+    if num5 > max_value:
+        max_value = num5
+
+    print(max_value)
 
 
-if __name__ == "__main__":
-    main()
+
+
+
+
+
+find_min_max()
