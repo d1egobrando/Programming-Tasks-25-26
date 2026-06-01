191a9eeeae422b86e76a3216c2a473e19b0efb92
d1egobrando <45485@rws.uk.net>
2026-06-01 22:18:20 +0100

    calc
    Refactored grade calculation logic and added user input.

--- Week_02_GCSE_Basics/01_Grade_Calculation.py
+++ Week_02_GCSE_Basics/01_Grade_Calculation.py
@@ -1,25 +1,15 @@
-"""
-TASK: 01 Grade Calculation
+def grade_calc(score):
+    if score >= 80 and score <= 100:
+        return "A"
+    elif score >= 60:
+        return "B"
+    elif score >= 40:
+        return "C"
+    else:
+        return "D"
 
-# Skills: Input, output, selection
-Write a program that asks the user for a percentage grade and prints the corresponding letter grade:
-- A: 80-100
-- B: 60-79
-- C: 40-59
-- D: <40
-Include a function def get_grade(score):
+percentage = int(input("Enter your percentage grade: "))
 
-TODO:
-- Fill in functions
-- Add demonstration code under `if __name__ == "__main__":`
-"""
+grade = grade_calc(percentage)
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
-
-
-if __name__ == "__main__":
-    main()
+print("Your grade is:", grade)
