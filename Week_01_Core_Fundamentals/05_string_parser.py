2131095d445e04186752be96d858c5c0546bbf87
d1egobrando <45485@rws.uk.net>
2026-06-01 10:40:36 +0100

    Split sentence

--- Week_01_Core_Fundamentals/05_string_parser.py
+++ Week_01_Core_Fundamentals/05_string_parser.py
@@ -1,23 +1,15 @@
-"""
-TASK: 05 String Parser
+your_sentence = input("Enter your sentence: ")
 
-# String Parser
-Write a parser that:
-- Accepts a sentence from the user.
-- Splits it into words manually (not using split()).
-- Outputs number of words + list of words.
+words = []
+word = ""
 
-TODO:
-- Fill in functions
-- Add demonstration code under `if __name__ == "__main__":`
-"""
+for character in your_sentence:
+    if character != " ":
+        word = word + character
+    else:
+        words.append(word)
+        word = ""
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
+words.append(word)
 
-
-if __name__ == "__main__":
-    main()
+print("Words:", words)
