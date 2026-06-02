19fffacca5b375950719afe0b6db1febc51d98df
d1egobrando <45485@rws.uk.net>
2026-06-02 11:34:36 +0100

    edit

--- Week_02_GCSE_Basics/05_Shopping_List.py
+++ Week_02_GCSE_Basics/05_Shopping_List.py
@@ -12,3 +12,17 @@ print("\nShopping List:")
 
 for i in range(len(your_shopping_list)):
     print(i + 1, "-", your_shopping_list[i])
+
+edit = input("\nDo you want to edit an item? (y/n): ")
+
+if edit == "y":
+    item_number = int(input("Enter the item you want  to edit: "))
+
+    new_item = input("Enter the new item: ")
+
+    your_shopping_list[item_number - 1] = new_item
+
+    print("\nUpdated Shopping List:")
+
+    for i in range(len(your_shopping_list)):
+        print(i + 1, "-", your_shopping_list[i])
