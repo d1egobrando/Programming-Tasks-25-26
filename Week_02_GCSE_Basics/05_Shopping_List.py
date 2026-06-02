6d12a6bb652cbf589ae6c8de3b6484ac21820af9
d1egobrando <45485@rws.uk.net>
2026-06-02 11:19:46 +0100

    Shopping
    Implemented a shopping list feature that allows users to add items until they type 'finish', then displays the list.

--- Week_02_GCSE_Basics/05_Shopping_List.py
+++ Week_02_GCSE_Basics/05_Shopping_List.py
@@ -1,22 +1,14 @@
-"""
-TASK: 05 Shopping List
+your_shopping_list = []
 
-# Skills: Loops, lists
-Allow the user to add itemds to a shopping list until they type DONE
-When they type DONE, print the list and ask if they want to edit any item.
-They should select an item by number and allow them to ammend the item.
+item = ""
 
-TODO:
-- Fill in functions
-- Add demonstration code under `if __name__ == "__main__":`
-"""
+while item != "finish":
+    item = input("Enter an item (or finish to END): ")
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
+    if item != "finish":
+        your_shopping_list.append(item)
 
+print("\nShopping List:")
 
-if __name__ == "__main__":
-    main()
+for i in range(len(your_shopping_list)):
+    print(i + 1, "-", your_shopping_list[i])
