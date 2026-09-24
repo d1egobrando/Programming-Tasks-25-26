dd61494783a9bbd32fbeb5b2a59d3747c21efe12
d1egobrando <45485@rws.uk.net>
2026-09-24 22:22:04 +0100

    Refactor input handling and add name deletion feature

--- Week_03_Arrays_Lists_CSV_Basics/03_2D_Array_Adder.py
+++ Week_03_Arrays_Lists_CSV_Basics/03_2D_Array_Adder.py
@@ -5,11 +5,22 @@ while True:
     print("2. Read Names")
     print("3. Delete Names")
     print("4. Exit")
-    
+
     choice = input("Enter your choice: ")
-    
+
         if choice == "1":
             name = input("Enter your name: ")
             age = input("Enter your age: ")
-            
+
             people.append([name,age])
+
+        elif choice == "2":
+            for person in people:
+                print("Name: ",person[0], ", Age: ",person[1])
+
+        elif choice == "3":
+            num = int(input("Enter the number of a name to deelete them "))
+            people.pop(num)
+
+        elif choice == "4":
+            break
