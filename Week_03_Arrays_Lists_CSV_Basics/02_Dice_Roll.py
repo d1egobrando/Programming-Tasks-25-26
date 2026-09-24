cc43c7200c341b198de4718804275a65a0f58ed9
d1egobrando <45485@rws.uk.net>
2026-09-24 21:36:52 +0100

    Implement dice rolling simulation
    Simulate rolling a die and count occurrences.

--- Week_03_Arrays_Lists_CSV_Basics/02_Dice_Roll.py
+++ Week_03_Arrays_Lists_CSV_Basics/02_Dice_Roll.py
@@ -1,26 +1,14 @@
-"""
-TASK: 02 Dice Roll
+import random
 
-# Skills: RNG, Loops
-Simulate rolling a six-sided die X number of times:
-Print each roll, store all values in a list of updated totals for each number (56 ones for example):
-Allow the user to print:
-- Totals for each side
-- average dice roll
-- Counts for each of the 6 sides
-- Extend (look up how to use mathplotlib and produce a bar graph for all of the statistics)
+rolling = int(input("How many times do you want to roll?"))
+num_available = [0, 0, 0, 0, 0, 0]
+every_roll = []
 
-TODO:
-- Fill in functions
-- Add demonstration code under `if __name__ == "__main__":`
-"""
+for i in range(rolling):
+    dice = random.randint(1, 6)
+    print(dice)
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
+    every_roll.append(dice)
+    num_available[dice] += 1
 
-
-if __name__ == "__main__":
-    main()
+print("Total Amount of sides", num_available)
