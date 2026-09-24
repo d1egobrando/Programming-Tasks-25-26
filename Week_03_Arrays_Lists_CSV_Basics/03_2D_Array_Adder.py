adcc8625b14d6fc202350b8e3ca095b87b8241f0
d1egobrando <45485@rws.uk.net>
2026-09-24 22:15:39 +0100

    Update 03_2D_Array_Adder.py

--- Week_03_Arrays_Lists_CSV_Basics/03_2D_Array_Adder.py
+++ Week_03_Arrays_Lists_CSV_Basics/03_2D_Array_Adder.py
@@ -1,1 +1,15 @@
+people = []
 
+while True:
+    print("\n1. Add name")
+    print("2. Read Names")
+    print("3. Delete Names")
+    print("4. Exit")
+    
+    choice = input("Enter your choice: ")
+    
+        if choice == "1":
+            name = input("Enter your name: ")
+            age = input("Enter your age: ")
+            
+            people.append([name,age])
