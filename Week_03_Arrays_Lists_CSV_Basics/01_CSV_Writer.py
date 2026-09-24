53598bd194a871d537b346110731fac33f12e727
d1egobrando <45485@rws.uk.net>
2026-09-24 21:24:49 +0100

    Change file mode from 'append' to 'a' in CSV writer

--- Week_03_Arrays_Lists_CSV_Basics/01_CSV_Writer.py
+++ Week_03_Arrays_Lists_CSV_Basics/01_CSV_Writer.py
@@ -4,6 +4,6 @@ favcolour = str(input("What is your favourite colour?:"))
 favfruit = str(input("What is your favourite fruit?:"))
 favsubject = str(input("What is your favourite subject?:"))
 
-file = open("csvwrite.csv", "append")
+file = open("csvwrite.csv", "a")
 file.write("name,age,favcolour,favfruit,favsubject\n")
 file.close()
