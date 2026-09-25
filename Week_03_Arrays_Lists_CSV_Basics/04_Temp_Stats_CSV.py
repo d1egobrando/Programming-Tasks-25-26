a50c746064a6dab01676a0b2750224d70cc523cf
d1egobrando <45485@rws.uk.net>
2026-09-25 11:26:33 +0100

    Open meantemp_daily_totals.txt for reading
    Added file opening for meantemp_daily_totals.txt.

--- Week_03_Arrays_Lists_CSV_Basics/04_Temp_Stats_CSV.py
+++ Week_03_Arrays_Lists_CSV_Basics/04_Temp_Stats_CSV.py
@@ -7,3 +7,5 @@ averagetemp = sum(temperatures) / len(temperatures)
 print("The Highest temperature is:", highesttemp)
 print("The Lowest temperature is:", lowesttemp)
 print("The Average temperature is:", averagetemp)
+
+file = open("meantemp_daily_totals.txt", "r") #read
